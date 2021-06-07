3147c991678df24191529412268d5d380aabb1ea
iliana etaoin <iliana@buttslol.net>
2021-06-07 13:26:56 -0700

    chronicler request refactor

--- src/chronicler.rs
+++ src/chronicler.rs
@@ -1,17 +1,66 @@
+use anyhow::Result;
 use chrono::{DateTime, SecondsFormat, Utc};
-use serde::Deserialize;
+use derive_builder::Builder;
+use reqwest::Response;
+use serde::{Deserialize, Serialize};
+use std::fmt;
 
-pub fn iso8601(t: DateTime<Utc>) -> String {
-    t.to_rfc3339_opts(SecondsFormat::AutoSi, true)
+#[derive(Debug, Default, Serialize, Builder)]
+pub struct Request {
+    #[builder(setter(into))]
+    #[serde(skip)]
+    route: String,
+    #[builder(setter(into, strip_option))]
+    #[serde(rename = "type")]
+    ty: Option<String>,
+    #[builder(setter(strip_option))]
+    count: Option<usize>,
+    #[builder(setter(strip_option))]
+    order: Option<Order>,
+    #[builder(setter(strip_option))]
+    after: Option<DateTime<Utc>>,
+    #[builder(setter(strip_option))]
+    before: Option<DateTime<Utc>>,
 }
 
-pub fn url(s: &str) -> String {
-    format!(
-        "{}{}",
-        option_env!("CHRONICLER_BASE_URL").unwrap_or("https://api.sibr.dev/chronicler/"),
-        s
-    )
+impl RequestBuilder {
+    pub async fn send(&self) -> Result<Response> {
+        let request = self.build()?;
+        Ok(reqwest::get(format!(
+            "{}{}?{}",
+            option_env!("CHRONICLER_BASE_URL").unwrap_or("https://api.sibr.dev/chronicler/"),
+            &request.route,
+            serde_urlencoded::to_string(&request)?
+        ))
+        .await?)
+    }
 }
+
+impl RequestBuilder {
+    pub fn new<I: Into<String>>(route: I) -> RequestBuilder {
+        let mut builder = RequestBuilder::default();
+        builder.route(route);
+        builder
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum Order {
+    Ascending,
+    Descending,
+}
+
+impl fmt::Display for Order {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Order::Ascending => "asc",
+            Order::Descending => "desc",
+        }
+        .fmt(f)
+    }
+}
+
+serde_plain::derive_serialize_from_display!(Order);
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
--- src/site.rs
+++ src/site.rs
@@ -1,19 +1,22 @@
-use crate::chronicler::{self, iso8601, Paginated, SiteUpdate};
+use crate::chronicler::{Order, Paginated, RequestBuilder, SiteUpdate};
 use anyhow::Result;
 use chrono::{DateTime, Utc};
 use reqwest::{Response, Url};
 
 pub async fn get(path: &str, at: DateTime<Utc>) -> Result<Option<Response>> {
     // TODO cache `v1/site/updates`
-    let url = Url::parse_with_params(
-        &chronicler::url("v1/site/updates?order=desc"),
-        &[("before", iso8601(at))],
-    )?;
-    let response: Paginated<SiteUpdate> = reqwest::get(url).await?.json().await?;
+    let response: Paginated<SiteUpdate> = RequestBuilder::new("v1/site/updates")
+        .order(Order::Descending)
+        .before(at)
+        .send()
+        .await?
+        .json()
+        .await?;
     Ok(
         match response.data.into_iter().find(|update| update.path == path) {
             Some(update) => Some(
-                reqwest::get(chronicler::url(&format!("v1{}", update.download_url)))
+                RequestBuilder::new(format!("v1{}", update.download_url))
+                    .send()
                     .await?
                     .error_for_status()?,
             ),
