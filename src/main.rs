b6daf72e09cd1a7991d44961e47294c61c2399d0
iliana etaoin <iliana@buttslol.net>
2021-06-07 10:33:41 -0700

    /api/getUser and /api/getUserRewards

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,29 @@ use crate::time::OffsetTime;
 use anyhow::anyhow;
 use either::Either;
 use rocket::request::FromRequest;
-use rocket::response::status::NotFound;
+use rocket::response::content::Json;
+use rocket::response::status::{NoContent, NotFound};
 use rocket::{catch, catchers, get, launch, routes, Request};
+use serde_json::json;
 use std::path::PathBuf;
 
 type Result<T> = std::result::Result<T, rocket::response::Debug<anyhow::Error>>;
+
+#[get("/api/getUser")]
+fn get_user() -> Json<serde_json::Value> {
+    Json(json!({
+        "id": "be457c4e-79e6-4016-94f5-76c6705741bb",
+        "email": "before@sibr.dev",
+        "verified": true,
+        "snacks": [],
+        "snackOrder": [],
+    }))
+}
+
+#[get("/api/getUserRewards")]
+fn get_user_rewards() -> NoContent {
+    NoContent
+}
 
 #[get("/static/<path..>")]
 async fn site_static(path: PathBuf, time: OffsetTime) -> Result<Option<Proxy>> {
@@ -57,6 +75,6 @@ async fn index_default(req: &Request<'_>) -> Result<Either<Proxy, NotFound<()>>>
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![site_static, index])
+        .mount("/", routes![get_user, get_user_rewards, site_static, index])
         .register("/", catchers![index_default])
 }
