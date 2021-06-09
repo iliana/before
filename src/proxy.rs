76ff681cd0337ec85a5761ad943b13f597124c92
iliana etaoin <iliana@buttslol.net>
2021-06-08 23:28:02 -0700

    set content-length on proxied requests

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,5 +1,6 @@
 use rocket::futures::TryStreamExt;
-use rocket::http::{ContentType, Status};
+use rocket::http::hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
+use rocket::http::{ContentType, Header, Status};
 use rocket::request::Request;
 use rocket::response::{Responder, Response, Result};
 use std::str::FromStr;
@@ -10,16 +11,26 @@ pub struct Proxy(pub reqwest::Response);
 
 impl<'r> Responder<'r, 'static> for Proxy {
     fn respond_to(self, _: &'r Request<'_>) -> Result<'static> {
-        Response::build()
+        let mut builder = Response::build();
+        builder
             .status(Status::new(self.0.status().as_u16()))
             .header(
                 self.0
                     .headers()
-                    .get(reqwest::header::CONTENT_TYPE)
+                    .get(CONTENT_TYPE)
                     .and_then(|s| s.to_str().ok())
                     .and_then(|s| ContentType::from_str(s).ok())
                     .unwrap_or(ContentType::Binary),
-            )
+            );
+        if let Some(len) = self
+            .0
+            .headers()
+            .get(CONTENT_LENGTH)
+            .and_then(|s| s.to_str().ok())
+        {
+            builder.header(Header::new(CONTENT_LENGTH.as_str(), len.to_owned()));
+        }
+        builder
             .streamed_body(
                 self.0
                     .bytes_stream()
