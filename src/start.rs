d2cfd541e235f695c67e7563909acd22f66c7ea3
iliana etaoin <iliana@buttslol.net>
2021-08-04 18:28:49 -0700

    coffee cup + season 12 bookmarks

--- src/start.rs
+++ src/start.rs
@@ -1,11 +1,12 @@
 use crate::Result;
 use askama::Template;
-use chrono::{DateTime, Utc};
 use rocket::get;
 use rocket::response::content::Html;
 use serde::Deserialize;
+use std::collections::HashMap;
+use toml::Value;
 
-const JUMP_BASE: &str = "/_before/jump?redirect=%2F";
+const JUMP_BASE: &str = "/_before/jump";
 
 #[derive(Deserialize, Template)]
 #[template(path = "start.html")]
@@ -47,39 +48,55 @@ struct ExtraTitle {
 #[derive(Deserialize)]
 struct Event {
     title: String,
-    time: Option<DateTime<Utc>>,
-    tournament: Option<i64>,
-    day: Option<i64>,
+    being: Option<Being>,
+    #[serde(flatten)]
+    jump_args: HashMap<String, Value>,
 }
 
 impl Event {
+    fn class(&self) -> String {
+        match self.being.as_ref() {
+            Some(being) => format!("bigdeal bigdeal-{}", *being as i8),
+            None => String::new(),
+        }
+    }
+
     fn jump(&self) -> String {
-        self.season_jump(&Season::default())
+        let mut args = self.jump_args.clone();
+        args.entry("redirect".to_string())
+            .or_insert_with(|| Value::from("/league"));
+        format!(
+            "{}?{}",
+            JUMP_BASE,
+            serde_urlencoded::to_string(&args).unwrap()
+        )
     }
 
     fn season_jump(&self, season: &Season) -> String {
-        if let Some(time) = self.time {
-            format!("{}&time={}", JUMP_BASE, time.to_rfc3339())
-        } else if let Some(day) = self.day {
-            if let Some(tournament) = self.tournament {
-                format!(
-                    "{base}&tournament={tournament}&day={day}",
-                    base = JUMP_BASE,
-                    tournament = tournament,
-                    day = day,
-                )
-            } else {
-                format!(
-                    "{base}&season={season}&day={day}",
-                    base = JUMP_BASE,
-                    season = season.number,
-                    day = day,
-                )
-            }
-        } else {
-            String::new()
-        }
+        let mut args = self.jump_args.clone();
+        args.entry("redirect".to_string())
+            .or_insert_with(|| Value::from("/league"));
+        args.entry("season".to_string())
+            .or_insert_with(|| Value::from(season.number));
+        format!(
+            "{}?{}",
+            JUMP_BASE,
+            serde_urlencoded::to_string(&args).unwrap()
+        )
     }
+}
+
+#[derive(Deserialize, Clone, Copy)]
+#[serde(rename_all = "lowercase")]
+enum Being {
+    Alert = -1,
+    Peanut = 0,
+    Monitor = 1,
+    Coin = 2,
+    Reader = 3,
+    Parker = 4,
+    Lootcrates = 5,
+    Namerifeht = 6,
 }
 
 #[get("/_before/start", rank = 1)]
