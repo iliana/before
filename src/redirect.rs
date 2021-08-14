36f3a1cadc4844690fff288dab81457203f4f094
iliana etaoin <iliana@buttslol.net>
2021-08-13 22:48:01 -0700

    fix nav form redirect on metapages

--- src/redirect.rs
+++ src/redirect.rs
@@ -16,7 +16,12 @@ impl<'r> Responder<'r, 'static> for Redirect {
             .get_one(REFERER.as_str())
             .and_then(|s| Reference::parse(s).ok())
         {
-            Redir::to(referer.path().as_str().to_owned())
+            let referer = referer.path().as_str();
+            if referer.starts_with("/_before") {
+                Redir::to(uri!(crate::site::index))
+            } else {
+                Redir::to(referer.to_owned())
+            }
         } else {
             Redir::to(uri!(crate::site::index))
         }
