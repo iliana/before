0697b37b9c0457987d3529d1200a28eb190e7b3e
iliana etaoin <iliana@buttslol.net>
2022-01-09 12:41:39 -0800

    wip

---
+++ build.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("cargo:rerun-if-changed=package-lock.json");
+    println!("cargo:rerun-if-changed=postcss.config.js");
+    println!("cargo:rerun-if-changed=src/");
+    println!("cargo:rerun-if-changed=tailwind.config.js");
+    println!("cargo:rerun-if-changed=templates/");
+
+    if !std::process::Command::new("npx")
+        .env("NODE_ENV", "production")
+        .arg("postcss")
+        .arg("src/styles.css")
+        .arg("-o")
+        .arg("static/styles.css")
+        .status()
+        .unwrap()
+        .success()
+    {
+        println!("cargo:warning=failed to run postcss");
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./templates/*.{html,js,svg}", "./src/**/*.rs"],
+  prefix: "tw-",
+  theme: {
+    fontFamily: {
+      serif: ["Lora", "Courier New", "monospace", "serif"],
+      sans: ["Open Sans", "Helvetica Neue", "sans-serif"],
+    },
+  },
+};
