09db223785dd8af01f87d78d03e9f090dd4c991a
iliana etaoin <iliana@buttslol.net>
2022-01-09 22:30:21 -0800

    wip wip wip

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: {
+    "postcss-nested": {},
     tailwindcss: {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: ["./templates/*.{html,js,svg}", "./src/**/*.rs"],
+  important: ".tw-before-scope",
   prefix: "tw-",
+  content: ["./templates/*.{html,js,svg}", "./src/**/*.rs"],
   theme: {
+    extend: {
+      colors: {
+        gray: colors.neutral,
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.25rem",
+        lg: "1.75rem",
+      },
+    },
     fontFamily: {
       serif: ["Lora", "Courier New", "monospace", "serif"],
       sans: ["Open Sans", "Helvetica Neue", "sans-serif"],
+    },
+    screens: {
+      lg: "1080px",
     },
   },
 };
