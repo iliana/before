f507859ce03c3fbdd170fe5599326018b6bc93c0
iliana etaoin <iliana@buttslol.net>
2022-02-20 23:36:05 -0800

    clean up next.js build manifests

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use cmd_lib::run_cmd;
 use std::env;
 use std::fs;
+use std::path::Path;
 
 fn main() -> Result<()> {
     cmd_lib::set_pipefail(true);
@@ -24,6 +25,15 @@ fn main() -> Result<()> {
         fs::remove_dir_all("out/_next/data")?;
         fs::remove_dir_all("out/_next/static/chunks")?;
         fs::remove_file("out/404.html")?;
+
+        for entry in fs::read_dir("out/_next")? {
+            let name = entry?.file_name();
+            if name != "static" {
+                fs::remove_dir_all(Path::new("out/_next").join(&name))?;
+                fs::remove_dir_all(Path::new("out/_next/static").join(&name))?;
+                break;
+            }
+        }
     }
 
     Ok(())
