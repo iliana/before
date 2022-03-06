1c7f6474fdeefb95e92fc27ac8bffd999a65c2e5
iliana etaoin <iliana@buttslol.net>
2022-03-06 10:21:51 -0800

    remove <meta name="description">

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Nav from "../components/nav";
 import "../styles/styles.css";
 
@@ -5,6 +6,9 @@ export default function App({ Component, pageProps }) {
   const navProps = Component.beforeNavProps ?? { open: true, hideSkip: true };
   return (
     <>
+      <Head>
+        <title>Before</title>
+      </Head>
       <Nav {...navProps} />
       <Component {...pageProps} />
     </>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,8 +6,6 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/static/media/favicon-32x32.png" />
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="&#x1FA78; Do you remember before? &#x1FA78;" />
-        <title>Before</title>
       </Head>
       <body className="tw-bg-black tw-text-white tw-text-dev lg:tw-text-lg">
         <Main />
