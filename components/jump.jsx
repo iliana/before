ef09bc878361420aa01ee61b00074a4db9b941f4
iliana etaoin <iliana@buttslol.net>
2022-03-08 09:03:33 -0800

    Jump: better default redirects

--- components/jump.jsx
+++ components/jump.jsx
@@ -3,19 +3,23 @@ import React, { useContext } from "react";
 export const JumpDefaults = React.createContext({});
 
 export function Jump({ children, className, ...jump }) {
-  const defaults = useContext(JumpDefaults);
-  if (!jump) {
+  const params = { ...useContext(JumpDefaults), ...jump };
+
+  if (
+    params.time === undefined &&
+    (params.day === undefined || (params.season === undefined && params.tournament === undefined))
+  ) {
     return children;
   }
 
-  const params = new URLSearchParams({
-    redirect: jump.season && jump.season > 11 ? "/league" : "/",
-    ...defaults,
-    ...jump,
-  });
+  if (params.redirect === undefined) {
+    const expansion =
+      (params.season && params.season >= 12) || (params.time && new Date(params.time).getTime() >= 1614571800000);
+    params.redirect = expansion ? "/league" : "/";
+  }
 
   return (
-    <a className={className} href={`/_before/jump?${params}`}>
+    <a className={className} href={`/_before/jump?${new URLSearchParams(params)}`}>
       {children}
     </a>
   );
