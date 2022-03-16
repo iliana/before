757a185ecbc18cbb1453fa3015cdcf5481f948a7
iliana etaoin <iliana@buttslol.net>
2022-03-15 17:33:08 -0700

    linescore overflow fade

--- components/linescore.jsx
+++ components/linescore.jsx
@@ -1,47 +1,73 @@
 /* eslint-disable react/no-array-index-key */
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { getTeam } from "../lib/teams";
 import { LineScoreData } from "../pages/_app";
 import Image from "./image";
 import salmonIcon from "./salmon.png";
 
+function overflowCheck({ target }) {
+  const { classList } = target.parentElement;
+  if (target.scrollWidth > target.offsetWidth) {
+    if (target.scrollLeft > 0) {
+      classList.add("tw-before-overflow-left");
+    } else {
+      classList.remove("tw-before-overflow-left");
+    }
+    if (target.scrollLeft < target.scrollWidth - target.offsetWidth) {
+      classList.add("tw-before-overflow-right");
+    } else {
+      classList.remove("tw-before-overflow-right");
+    }
+  }
+}
+
 export default function LineScore({ id, salmon = {} }) {
   const { season, day, away, home } = useContext(LineScoreData)[id];
   return (
-    <div className="tw-w-full tw-max-w-full tw-overflow-x-auto tw-my-2 lg:tw-my-3">
-      <table className="tw-font-sans tw-whitespace-nowrap tw-text-sm lg:tw-text-base tw-mx-auto">
-        <thead>
-          <tr className="tw-text-xs lg:tw-text-sm">
-            <td />
-            <td className="tw-font-bold tw-pr-1 lg:tw-pr-2">
-              S{season}/{day}
-            </td>
-            {away.innings.map((_1, index) =>
-              ((salmon.away ?? {})[index + 1] ?? [0]).map((_2, salmonIndex) => (
-                <Header key={`${index}.${salmonIndex}`}>
-                  {salmonIndex % 2 ? (
-                    <div className="tw-h-2.5 lg:tw-h-3.5 tw-px-0.5 lg:tw-px-1">
-                      <Image alt="Salmon" src={salmonIcon} />
-                    </div>
-                  ) : (
-                    index + 1
-                  )}
-                </Header>
-              ))
-            )}
-            <Header>
-              <abbr title="Runs">R</abbr>
-            </Header>
-            <Header>
-              <abbr title="Hits">H</abbr>
-            </Header>
-          </tr>
-        </thead>
-        <tbody>
-          <LineScoreRow team={away} innings={away.innings.length} salmon={salmon.away ?? {}} />
-          <LineScoreRow team={home} innings={away.innings.length} salmon={salmon.home ?? {}} />
-        </tbody>
-      </table>
+    <div className="tw-before-linescore-fade">
+      <div
+        className="tw-w-full tw-max-w-full tw-overflow-x-auto tw-my-2 lg:tw-my-3"
+        onScroll={overflowCheck}
+        ref={useCallback((node) => {
+          if (node) {
+            node.dispatchEvent(new Event("scroll"));
+          }
+        }, [])}
+      >
+        <table className="tw-font-sans tw-whitespace-nowrap tw-text-sm lg:tw-text-base tw-mx-auto">
+          <thead>
+            <tr className="tw-text-xs lg:tw-text-sm">
+              <td />
+              <td className="tw-font-bold tw-pr-1 lg:tw-pr-2">
+                S{season}/{day}
+              </td>
+              {away.innings.map((_1, index) =>
+                ((salmon.away ?? {})[index + 1] ?? [0]).map((_2, salmonIndex) => (
+                  <Header key={`${index}.${salmonIndex}`}>
+                    {salmonIndex % 2 ? (
+                      <div className="tw-h-2.5 lg:tw-h-3.5 tw-px-0.5 lg:tw-px-1">
+                        <Image alt="Salmon" src={salmonIcon} />
+                      </div>
+                    ) : (
+                      index + 1
+                    )}
+                  </Header>
+                ))
+              )}
+              <Header>
+                <abbr title="Runs">R</abbr>
+              </Header>
+              <Header>
+                <abbr title="Hits">H</abbr>
+              </Header>
+            </tr>
+          </thead>
+          <tbody>
+            <LineScoreRow team={away} innings={away.innings.length} salmon={salmon.away ?? {}} />
+            <LineScoreRow team={home} innings={away.innings.length} salmon={salmon.home ?? {}} />
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
